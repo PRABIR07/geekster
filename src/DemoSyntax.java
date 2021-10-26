b987bdaee4310b3c1476d777f10c90ae9171fcc4
PRABIR07 <uic.19mca1007@gmail.com>
2021-10-26 20:54:40 +0530

    java Program

---
+++ src/DemoSyntax.java
@@ -0,0 +1,17 @@
+
+public class DemoSyntax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       DemoSyntax obj= new DemoSyntax();
+        obj.show();
+        Demosyntax2 ab= new Demosyntax2();
+        ab.Add();
+	}
+	
+	 void show() {
+		 System.out.println("Hello");
+		 
+	 }
+
+}
